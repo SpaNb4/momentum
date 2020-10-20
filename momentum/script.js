4bd412d34d696070d6ab21846eae106320dee6e7
SpaNb4 <spanb4@gmail.com>
2020-10-20 14:36:28 +0300

    feat: add quote

--- momentum/script.js
+++ momentum/script.js
@@ -1,22 +1,30 @@
 // DOM Elements
 const body = document.querySelector('body');
-const btn = document.querySelector('.btn');
+const btn_bg_change = document.querySelector('.btn_bg_change');
 const date = document.querySelector('.date');
 const time = document.querySelector('.time');
 const greeting = document.querySelector('.greeting');
 const name = document.querySelector('.name');
 const focus = document.querySelector('.focus');
 
+const btn_quote_change = document.querySelector('.btn_quote_change');
+const blockquote = document.querySelector('blockquote');
+const figcaption = document.querySelector('figcaption');
+
 let day_period = '';
 let base = '';
 const images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg'];
 const day_periods = ['morning', 'day', 'evening', 'night'];
+let current_day_period = 0;
 let i = 0;
 let n = 1;
 
 // Show Time
 function showTime() {
     let today = new Date();
+    // today.setHours(today.getHours() + 5);
+    today.setMinutes(today.getMinutes() + 107);
+    today.setSeconds(today.getSeconds() + 20);
     let day_week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
     let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
@@ -68,6 +76,7 @@ function setBgGreet() {
         greeting.textContent = 'Доброй Ночи,';
     }
 
+    current_day_period = day_periods.indexOf(day_period)
     base = `assets/images/${day_period}/`;
 }
 
@@ -109,7 +118,7 @@ function getFocus() {
 function setFocus(e) {
     if (e.type === 'keypress') {
         // Make sure enter is pressed
-        if (e.which == 13 || e.keyCode == 13) {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
             localStorage.setItem('focus', e.target.innerText);
             focus.blur();
         }
@@ -128,18 +137,32 @@ function viewBgImage(data) {
     };
 }
 
+const generator = function* (arr) {
+    let i = current_day_period + 1;
+    let length = arr.length;
+    while (true) {
+        yield arr[i];
+        i++;
+        if (i === length) {
+            i = 0
+        }
+    }
+}
+
+const day_period_loop = generator(day_periods);
+
 function getImage(all) {
     let imageSrc = base + images[i];
     viewBgImage(imageSrc);
-    console.log(day_periods.indexOf(day_period))
-    if (all) {
-        if (i == images.length - 1) {
-            i = -1;
 
-            base = `assets/images/${day_periods[day_periods.indexOf(day_period) + n]}/`;
+    if (all) {
+        i++;
+        if (i >= images.length) {
+            i = 0;
+            base = `assets/images/${day_period_loop.next().value}/`;
             imageSrc = base + images[i];
         }
-        i++;
+
     }
     else {
         if (i < images.length - 1) {
@@ -149,6 +172,15 @@ function getImage(all) {
             i = 0;
         }
     }
+}
+
+function getQuote() {
+    fetch('https://cors-anywhere.herokuapp.com/http://api.quotable.io/random')
+        .then(response => response.json())
+        .then(data => {
+            blockquote.textContent = data.content;
+            figcaption.textContent = data.author;
+        })
 }
 
 name.addEventListener('keypress', setName);
@@ -173,10 +205,14 @@ focus.addEventListener('focusout', () => {
     }
 });
 
-btn.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', getQuote);
+
+btn_bg_change.addEventListener('click', () => {
     let all_img = true;
     getImage(all_img);
 });
+
+btn_quote_change.addEventListener('click', getQuote);
 
 // Run
 showTime();
